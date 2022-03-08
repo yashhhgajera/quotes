94f42251444ea3741f7a955db3cb73f8c44ba2b5
Nikunj Dhola <nikunjdhola67@gmail.com>
2022-03-08 11:02:39 +0530

    create

--- src/app/pages/user/create/create.component.ts
+++ src/app/pages/user/create/create.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
+  textdata = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quod numquam assumenda repellat praesentium nesciunt quos quo temporibus odio quis ab excepturi tempore magni beatae iusto soluta, exercitationem aliquid, laudantium porro voluptas repellendus vero delectus dolore nam. Aut molestias, natus saepe voluptatum incidunt, quasi dolorum animi dolore mollitia quia quos tempore repellat libero maiores? Aspernatur doloribus dolor veniam repellat, sapiente autem perspiciatis numquam sunt animi eius nihil omnis, vitae optio molestias, nostrum fugit ad voluptates laudantium reprehenderit consectetur error deserunt. Possimus odio officia id earum sunt nulla perspiciatis nisi quam incidunt illum quasi dolor ullam harum beatae dolores numquam consequatur esse qui deserunt nam ad, et distinctio illo! Ipsam expedita dolorem totam, commodi cupiditate molestiae? Eligendi, nisi expedita deleniti porro vel rerum ab dolores quae ea eius, non praesentium? Iusto sunt dicta ut provident corporis quisquam reiciendis repudiandae illo deleniti quidem ea tenetur iure, vero facilis fugiat laborum eveniet sit numquam doloremque pariatur tempora velit delectus. Expedita rem voluptate voluptates natus laboriosam impedit illum dignissimos eaque necessitatibus inventore, blanditiis dolores, eum saepe sequi. Cum repudiandae necessitatibus vel dolores labore consequatur delectus voluptate sint, fuga quidem asperiores repellendus dignissimos ullam incidunt accusantium placeat consequuntur cumque, ducimus, aperiam atque eveniet dicta. Quas!"
+
   constructor() { }
 
   ngOnInit(): void {
