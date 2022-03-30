03eaf3c3e0f3435fb3c8f00f595d0beef2e1a456
Yash Gajera <yashgajera6202@gmail.com>
2022-03-30 14:14:20 +0530

    admin routing setup

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { UserBlogComponent } from './user-blog/user-blog.component';
 import { UserListComponent } from './user-list/user-list.component';
 
 const routes: Routes = [
@@ -9,8 +10,17 @@ const routes: Routes = [
     component:AdminComponent,
     children:[
       {
-        path:'',
+        path:'users',
         component:UserListComponent
+      },
+      {
+        path:'blogs',
+        component:UserBlogComponent
+      },
+      {
+        path:'',
+        redirectTo:'users',
+        pathMatch:'full'
       }
     ]
   }
--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,16 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-admin',
-  template: `<router-outlet></router-outlet>`,
+  template: `
+  <button class="btn" routerLink="users" routerLinkActive="btn-dark">Users</button>
+  <button class="btn" routerLink="blogs" routerLinkActive="btn-dark">Blogs</button>
+  <button class="btn btn-danger" (click)="openModal(logout)"
+				
+				title="Logout">
+				<img src="../../../assets/icon_logout.svg"
+					alt="logout"></button>
+  <router-outlet></router-outlet>
+  <ng-template #logout>
+	<div class="modal-body text-center">
+		<p class="mt-1">Are you sure?</p>
+		<button (click)="confirm()"
+			type="button"
+			class="btn btn-default">
+			Yes
+		</button>
+		<button (click)="decline()"
+			type="button"
+			class="btn bg-black text-white">
+			No
+		</button>
+	</div>
+</ng-template>
+  `,
   styles: [
   ]
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  modalRef?: BsModalRef;
+
+  constructor(private router:Router, private modalService: BsModalService) { }
 
   ngOnInit(): void {
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  confirm(): void {
+    this.modalRef?.hide();
+    this.router.navigate(['./']);
+    localStorage.clear();
+  }
+
+  decline(): void {
+    this.modalRef?.hide();
   }
 
 }
